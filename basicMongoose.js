2d0d0f5f628576e0472a5a10a5ebec51fc258404
Himanshu <hnegi8984@gmail.com>
2022-07-24 12:27:12 +0530

    attempt1

--- basicMongoose.js
+++ basicMongoose.js
@@ -1,4 +1,4 @@
 const mongoose =require('mongoose');
-mongoose.connect(process.env.CONNECT_BASE,{useNewUrlParser: true}).then(
+mongoose.connect(process.env.CONNECT_BASE).then(
     console.log('its connected')
 );
